46923ec6aac52227e9f897ff1bebb4759542088e
Issac Kelly <issac.kelly@gmail.com>
2013-11-02 15:58:37 -0700

    Fix UserDict

--- riak/multidict.py
+++ riak/multidict.py
@@ -1,10 +1,10 @@
 # (c) 2005 Ian Bicking and contributors; written for Paste
 # (http://pythonpaste.org) Licensed under the MIT license:
 # http://www.opensource.org/licenses/mit-license.php
-from UserDict import DictMixin
+from collections import MutableMapping
 
 
-class MultiDict(DictMixin):
+class MultiDict(MutableMapping):
 
     """
     An ordered dictionary that can have multiple values for each key.
